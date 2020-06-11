43580e250592635860f07155eeab2830de0392df
TG <43038974+tamaghan@users.noreply.github.com>
2020-06-11 00:29:34 -0400

    Added Dashboard without functionality

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-skrollr.init();
-AOS.init();
 
+
+function show_meeting() {
+    var meet = document.getElementById('meetings');
+    var event = document.getElementById('events');
+    meet.style.display = "flex";
+    event.style.display = "none";
+}function show_event() {
+    var meet = document.getElementById('meetings');
+    var event = document.getElementById('events');
+    meet.style.display = "none";
+    event.style.display = "flex";
+}
 var submitButton = document.getElementById("submit_form");
     var form = document.getElementById("email_form");
     form.addEventListener("submit", function (e) {
